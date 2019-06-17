fd2913a59c9d765dd5e5aad394448b133d501a54
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-06-17 16:42:02 +0530

    'ReactMovies'

---
+++ src/app.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "../src/styles/app.css";
+import data from "./data";
+// import Menu from "./component/menu"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.jsonData = data;
+    this.state = {
+      show: false,
+      searchValues: this.jsonData.data,
+      genres: "",
+      title: "",
+      titleSelected: false,
+      genresSelected: false,
+      updatedList: {}
+    };
+    this.labelVal = "Title";
+    this.titleList = "";
+    this.genresList = "";
+  }
+
+  sortDescending = labelVal => {
+    const currentList = this.state.searchValues;
+    this.setState({
+      titleSelected: true
+    });
+    if (labelVal === "Title") {
+      this.labelVal = "Title";
+    } else {
+      this.labelVal = "Genres";
+    }
+    if (this.labelVal === "Title") {
+      this.titleList = currentList.sort((a, b) => {
+        if (a.title > b.title) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+      });
+
+      this.setState({
+        updatedList: this.titleList
+      });
+    } else {
+      this.genresList = currentList.sort((a, b) => {
+        if (a.genres > b.genres) {
+          return 1;
+        }
+        if (a.genres < b.genres) {
+          return -1;
+        }
+      });
+      //   this.genresList.sort((a, b)=>{a-b});
+      this.setState({
+        updatedList: this.genresList
+      });
+    }
+  };
+
+  handleChange = e => {
+    let newList = "";
+    const currentList = this.state.updatedList;
+    if (this.labelVal === "Title") {
+      if (e.target.value !== "") {
+        newList = currentList.filter(item => {
+          const filterValue = e.target.value;
+          return item.title.includes(filterValue);
+        });
+      } else {
+        newList = this.jsonData.data;
+      }
+    } else {
+      if (e.target.value !== "") {
+        newList = currentList.filter(item => {
+          const filterValue = e.target.value;
+          return item.genres.includes(filterValue);
+        });
+      } else {
+        newList = this.jsonData.data;
+      }
+    }
+    this.setState({
+      searchValues: newList
+    });
+  };
+  hoverDownMenu = () => {
+    this.setState({
+      show: false
+    });
+  };
+  render() {
+    const data = this.state.searchValues;
+    console.log(data);
+    const renderData = data.map((value, index) => {
+      return (
+        <ul className="gridContainer">
+          <li className="banner">
+            <img src={value.poster_path} />
+          </li>
+          <li className="title">{value.title}</li>
+          <li className="caption">{value.tagline}</li>
+        </ul>
+      );
+    });
+    return (
+      <div className="App mainCont">
+        <header className="searchBarContainer">
+          <div className="search">
+            <input
+              type="text"
+              id="search"
+              onChange={this.handleChange}
+              placeholder="Search..."
+            />
+            <button>Search</button>
+            <button
+              onClick={() => this.sortDescending((this.labelVal = "Genres"))}
+            >
+              {" "}
+              Genres{" "}
+            </button>
+            <button
+              onClick={() => this.sortDescending((this.labelVal = "Title"))}
+            >
+              {" "}
+              title{" "}
+            </button>
+          </div>
+          <div className="movieTitles">{renderData}</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+// const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+// const syntexDynamicImport = require('syntax-dynamic-import');
+module.exports = {
+    mode: 'development',
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+        new miniCssExtractPlugin({
+            filename: "./src/styles/app.scss",
+            chunkFilename: "./dist/app.css"
+        }),
+        // new LodashModuleReplacementPlugin
+        // new syntexDynamicImport
+    ],
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.html$/,
+                    use:[
+                        {
+                            loader: "html-loader",
+                            options: {minimize: true}
+                        }
+                    ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+}
